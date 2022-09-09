fc3799f4e15982264bcd1f15ff8f34e5db0ca56f
Ada <byjean94@gmail.com>
2022-09-09 11:39:57 +0200

    Install Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
+import { Button } from 'react-bootstrap';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+  return <div>
+    <Button>Hello</Button>
+  </div>
 }
 
 export default App;

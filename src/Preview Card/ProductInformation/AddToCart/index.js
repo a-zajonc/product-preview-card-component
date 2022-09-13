17b610d01d954a99232d77c4c6b64ca4790d787b
Ada <byjean94@gmail.com>
2022-09-13 19:50:40 +0200

    Remove unnecessary text element in Button

--- src/Preview Card/ProductInformation/AddToCart/index.js
+++ src/Preview Card/ProductInformation/AddToCart/index.js
@@ -1,14 +1,11 @@
-import { Button, Img, Text } from "@chakra-ui/react";
+import { Button, Img } from "@chakra-ui/react";
 
 export function AddToCart() {
 
-
     return <Button bg="#3C8067" width="100%" height="30px" p={{ base: "6", xl: "5" }} _hover={{
         bgColor: "#1a4031",
-    }}>
-        <Img src="icons8-shopping-cart-24.png" alt="Shopping cart" height="17px" />
-        <Text color="white" fontFamily="Montserrat" fontWeight="700" fontSize="14" p="3">
-            Add to Cart
-        </Text>
+    }} color="white" fontFamily="Montserrat" fontWeight="700" fontSize="14">
+        <Img src="icons8-shopping-cart-24.png" alt="Wózek zakupowy" height="17px" pr="3" />
+        Add to Cart
     </Button>
 }

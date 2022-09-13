533e1b6a9369b26fab894c28a99444e87deea691
Ada <byjean94@gmail.com>
2022-09-13 15:43:30 +0200

    Put Component in the Center of the Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-import { ChakraProvider, Box } from '@chakra-ui/react'
+import { ChakraProvider, Box, Center } from '@chakra-ui/react'
 import { PreviewCard } from './Preview Card';
 import { theme } from '.';
 
 function App() {
   return <ChakraProvider theme={theme}>
-    <Box height="100%" bgColor="#F2EBE3" p={{ base: '2', desktop: '10' }}>
+    <Box bgColor="#F2EBE3" height="100vh" display='flex' justifyContent='center' alignItems='center'>
       <PreviewCard />
     </Box>
   </ChakraProvider>
